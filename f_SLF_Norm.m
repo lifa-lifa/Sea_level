a874460466876283ff2e435a9c1c90604d7fd81d
LIFA <lifa@cowi.com>
2017-09-06 11:44:15 +0100

    LIFA copied sea level rise NORM function

--- f_SLF_Norm.m
+++ f_SLF_Norm.m
@@ -1,3 +1,5 @@
+% LIFA copied on 2017-09-06 from:
+% NEJO matlab function created for Nothern storms, 2017-04-01
 % This function return the normalized sea level rise
 % It deduct the isostatic movement of the coast line
 % and add the storm contribution (if any).
@@ -6,9 +8,12 @@
 % 
 function y=f_SLR_Norm(year,rateOfIsostaticAdjustmentMeter,...
                             rateOfStormContributionMeter)
+                        
     yyval=seaLevelRiseMeter(year,rateOfIsostaticAdjustmentMeter,...
                             rateOfStormContributionMeter);
+                        
     y2100val=seaLevelRiseMeter(2100,rateOfIsostaticAdjustmentMeter,...
                             rateOfStormContributionMeter);
+                        
     y=yyval/y2100val;
 end
