c2d3554007d5b1a3564bb0e4b20dbc324dd65e6b
LIFA <lifa@cowi.com>
2017-09-22 11:18:53 +0100

    UpperTail start

---
+++ StatSouth_1044_2013_UpperTail.m
@@ -0,0 +1,9 @@
+%% Oeresund A102102
+% * LIFA 2017-09-22
+% * Statistics for southern storms, upper segment (Exponential)
+% * Based on NEJO's Statistics South-1044-2013-UpperTail.nb
+close all
+clear all
+clc
+
+%% Inputs
